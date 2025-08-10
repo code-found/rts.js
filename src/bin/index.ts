48e4e18ff240f0dda63baca21d4989b43547d373
spencer <pallyoung@163.com>
2025-08-10 19:07:26 +0800

    feat: rts command support - use spawn to run ts files when call rts command

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 
+import { spawn } from "child_process";
 import { registerRTS } from "../index";
 
 // Register RTS hooks when this module is loaded
 registerRTS();
+
+spawn(`node`, ["-r", "rts.js/register", ...process.argv.slice(2)], {
+  cwd: process.cwd(),
+  stdio: "inherit",
+});
